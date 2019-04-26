a86b1a8c117ddfcf28a086087310c55e43e1e1bc
dasds <dasds>
2019-04-26 20:16:02 +0200

    k

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import com.github.kilianB.hash.Hash;
+import com.github.kilianB.hashAlgorithms.AverageHash;
 import com.github.kilianB.hashAlgorithms.DifferenceHash;
 import com.github.kilianB.hashAlgorithms.HashingAlgorithm;
 import com.github.kilianB.hashAlgorithms.PerceptiveHash;
@@ -9,12 +10,14 @@ import java.io.IOException;
 public class Main {
 
   public static void main(String[] args) throws IOException {
-    File org = new File("C:\\Users\\Pawel\\IdeaProjects\\DHash\\img\\org.jpg");
-    File phone = new File("C:\\Users\\Pawel\\IdeaProjects\\DHash\\img\\phone2.jpg");
-//    File phone = new File("C:\\Users\\Pawel\\IdeaProjects\\DHash\\img\\diff.jpg");
+    File org = new File("C:\\Users\\Pawel\\IdeaProjects\\DHash\\img\\korg.jpg");
+//    File phone = new File("C:\\Users\\Pawel\\IdeaProjects\\DHash\\img\\kfoto.jpg");
+    File phone = new File("C:\\Users\\Pawel\\IdeaProjects\\DHash\\img\\diff.jpg");
 
 //    HashingAlgorithm hasher = new PerceptiveHash(32);
-    HashingAlgorithm hasher = new DifferenceHash(64, DifferenceHash.Precision.Simple);
+//    HashingAlgorithm hasher = new DifferenceHash(64, DifferenceHash.Precision.Simple);
+    HashingAlgorithm hasher = new DifferenceHash(64, DifferenceHash.Precision.Triple);
+//    HashingAlgorithm hasher = new AverageHash(64);
 
     Hash hash0 = hasher.hash(org);
     Hash hash1 = hasher.hash(phone);
